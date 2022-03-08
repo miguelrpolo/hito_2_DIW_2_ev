148031477126d14d6d5c4cfd7cc03e1bcbb9d954
Miguel <55658607+miguelrpolo@users.noreply.github.com>
2022-03-08 12:04:03 +0100

    pdf añadido

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,6 +3,6 @@
 <title>Título descriptivo: Sitio Web</title>
 </head>
   <div class="about">
-      <h2>Título. No se puede modificar el titulo de la pestaña con title en Vue </h2>
+      <h2>Título. No se puede modificar el titulo de la pestaña con title en Vue</h2>
   </div>
 </template>
